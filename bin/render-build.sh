31bc3a8b3c6c19087b6841d2f32eaa6e7585315f
shiyuu <46603895+shiyuu33@users.noreply.github.com>
2025-05-29 17:03:31 +0000

    fix: update

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,5 +4,7 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+
+bundle exec rails runner "ActiveRecord::Base.connection_pool.disconnect!" 2>/dev/null || true
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
 bundle exec rails db:seed
